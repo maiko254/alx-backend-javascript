06abea5528384bf42fd649b710c7bbd6681db266
maiko254 <bonyomichael406@gmail.com>
2024-06-24 17:21:15 +0000

    Correction: Block scoped variables

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,8 +3,8 @@ export default function taskBlock(trueOrFalse) {
   var task2 = true;
 
   if (trueOrFalse) {
-    let task = true;
-    let task2 = false;
+    var task = false;
+    var task2 = true;
   }
 
   return [task, task2];
