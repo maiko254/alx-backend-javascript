f49600cca31bf8cad883e53a1112832dc9d7662d
maiko254 <bonyomichael406.com>
2024-06-27 17:57:04 +0300

    Class with a constructor, getters and setters for each attribute

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,31 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    if (typeof name === 'string') {
+      this._name = name;
+    }
+    if (typeof length === 'number') {
+      this._length = length;
+    }
+    if (Array.isArray(students) && students.every(item => typeof item === 'string')) {
+      this._students = students;
+    }
+  }
+  get name() {
+    return this._name;
+  }
+  set name(newname) {
+    this._name = newname;
+  }
+  get length() {
+    return this._length;
+  }
+  set length(newlength) {
+    this._length = newlength;
+  }
+  get students() {
+    return this._students;
+  }
+  set students(newstudents) {
+    this._students = newstudents;
+  }
+}
