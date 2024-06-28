c6bbd57f123e6cd0c40db280295c7c54a1bf74b4
maiko254 <bonyomichael406.com>
2024-06-28 21:35:44 +0300

    Function returning a Promise that resolves to an object

---
+++ 0x01-ES6_promise/4-user-promise.js
@@ -0,0 +1,10 @@
+export default function signUpUser(firstName, lastName) {
+  return new Promise((resolve) => {
+    const user = {
+      firstName,
+      lastName,
+    };
+
+    resolve(user);
+  });
+}
