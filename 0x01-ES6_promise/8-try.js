e7fad7ecb1de8b7e049a2a1b87f8f562a7ffccfa
maiko254 <bonyomichael406.com>
2024-06-29 00:01:05 +0300

    Correction: Used try/catch in the function

--- 0x01-ES6_promise/8-try.js
+++ 0x01-ES6_promise/8-try.js
@@ -1,9 +1,13 @@
 export default function divideFunction(numerator, denominator) {
   return new Promise((resolve, reject) => {
-    if (denominator === 0) {
-      reject(new Error('cannot divide by 0'));
-    } else {
+    try {
+      if (denominator === 0) {
+        throw new Error('cannot divide by 0');
+      }
+
       resolve(numerator / denominator);
+    } catch (error) {
+      reject(error);
     }
   });
 }
