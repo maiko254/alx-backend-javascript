4cd0fac6cd60badaf35d759de859cc575b0641df
maiko254 <bonyomichael406.com>
2024-07-05 16:21:42 +0300

    `feat`: Implement basic CRUD operations to save entities to a database in Typescript

---
+++ 0x04-TypeScript/task_3/js/main.ts
@@ -0,0 +1,28 @@
+import { RowID, RowElement } from "./interface";
+import * as CRUD from "./crud.js";
+
+/// <reference path="./crud.d.ts" />
+
+const row: RowElement = {
+    firstName: "Guillaume",
+    lastName: "Salva"
+};
+
+const newRowID: RowID = CRUD.insertRow(row);
+const updatedRow: RowElement = {
+    ...row,
+    age: 23
+};
+CRUD.updateRow(newRowID, updatedRow);
+CRUD.deleteRow(newRowID);
+
+const obj = {firstName: "Guillaume", lastName: "Salva"};
+CRUD.insertRow(obj)
+// Insert row {firstName: "Guillaume", lastName: "Salva"}
+
+const updatedRow: RowElement = { firstName: "Guillaume", lastName: "Salva", age: 23 };
+CRUD.updateRow(newRowID, updatedRow);
+// Update row 125 {firstName: "Guillaume", lastName: "Salva", age: 23}
+
+CRUD.deleteRow(125);
+// Delete row id 125
