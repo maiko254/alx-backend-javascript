e42715a8bc47a794682736faff1322089da38e1f
maiko254 <bonyomichael406.com>
2024-07-05 14:14:11 +0300

    Create Teacher interface and add to Subjects namespace then implement Cpp, Java, React classes with Teacher availability in Subject namespace

---
+++ 0x04-TypeScript/task_4/js/subjects/Cpp.ts
@@ -0,0 +1,19 @@
+namespace Subjects {
+    export interface Teacher {
+        experienceTeachingC?: number;
+    }
+
+    class Cpp extends Subjects.Subject{
+        getRequirements(): string {
+            return "Here is the list of requirements for Cpp";
+        }
+
+        getAvailableTeacher(): string {
+            if (this.teacher && this.teacher.experienceTeachingC) {
+                return `Available Teacher: ${this.teacher.firstName}`;
+            } else {
+                return "No available teacher";
+            }
+        }
+    }
+}
