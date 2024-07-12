3a6976e0dfa2ea934aa99e0333c9846f06fdbe6c
maiko254 <bonyomichael406.com>
2024-07-12 23:38:03 +0300

    Correction: Added check for type string

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,5 @@
 export default function cleanSet(set, startString) {
-  if (!startString) {
+  if (!startString || typeof startString !== 'string') {
     return '';
   }
   return Array.from(set).filter((str) => str && str.startsWith(startString)).map((str) => str.slice(startString.length)).join('-');
