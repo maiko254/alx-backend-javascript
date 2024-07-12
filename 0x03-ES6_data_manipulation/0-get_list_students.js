bb7d0a1e60909fe3e81ae703168f75995147ba96
maiko254 <bonyomichael406.com>
2024-07-12 14:31:02 +0300

    Correction

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,7 +1,7 @@
 export default function getListStudents() {
-  const list1 = {id: 1, firstName: 'Guillame', location: 'San Francisco'};
-  const list2 = {id: 2, firstName: 'James', location: 'Columbia'};
-  const list3 = {id: 5, firstName: 'Serena', location: 'San Francisco'};
-
-  return new Array(list1, list2, list3);
+  return [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  ];
 }
