d8e373bfd039a5a6989e89c687f768b4674c3d10
maiko254 <bonyomichael406.com>
2024-07-12 23:19:43 +0300

    Correction: checking if each value in set is string

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -2,5 +2,5 @@ export default function cleanSet(set, startString) {
   if (!startString) {
     return '';
   }
-  return Array.from(set).filter((str) => str.startsWith(startString)).map((str) => str.slice(startString.length)).join('-');
+  return Array.from(set).filter((str) => str && str.startsWith(startString)).map((str) => str.slice(startString.length)).join('-');
 }
