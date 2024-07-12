6d7b13baa6429863da604350f627542ad2274132
maiko254 <bonyomichael406.com>
2024-07-12 22:56:23 +0300

    Correction

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -2,16 +2,5 @@ export default function cleanSet(set, startString) {
   if (startString === '') {
     return '';
   }
-
-  let result = '';
-
-  for (const value of set) {
-    if (value.startsWith(startString)) {
-      result += `${value.slice(startString.length)}-`;
-    }
-  }
-
-  result = result.slice(0, -1);
-
-  return result;
+  return Array.from(set).filter((str) => str.startsWith(startString)).map((str) => str.slice(startString.length)).join('-');
 }
