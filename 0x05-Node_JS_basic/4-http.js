c553ff77c364fd86e6377048f94c44568c213de1
maiko254 <bonyomichael406.com>
2024-08-20 11:29:58 +0300

    simple Node server created with http module

---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,13 @@
+const { createServer } = require('node:http');
+
+const host = 'localhost';
+const port = 1245;
+
+const app = createServer((req, res) => {
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello Holberton School!');
+});
+
+app.listen(port, host, () => {});
+
+module.exports = app;
