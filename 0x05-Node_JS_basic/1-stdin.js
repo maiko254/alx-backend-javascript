307cfdcfd0535f2d3030493cccd92afe9e8addf1
maiko254 <bonyomichael406.com>
2024-08-20 23:20:49 +0300

    Correction

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,13 +1,13 @@
-console.log('Welcome to Holberton School, what is your name?');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.on('data', (data) => {
-  const name = data.toString().trim();
+process.stdin.on('readable', (data) => {
+  const chunk = process.stdin.read();
 
-  console.log(`Your name is: ${name}`);
-
-  process.exit();
+  if (chunk) {
+    process.stdout.write(`Your name is: ${chunk}`);
+  }
 });
 
 process.stdin.on('end', () => {
-  console.log('This important software is now closing');
+  process.stdout.write('This important software is now closing\n');
 });
