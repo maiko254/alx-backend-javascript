93c829ecaff58086f9b339a269815f64d87c1304
maiko254 <bonyomichael406.com>
2024-08-20 23:06:57 +0300

    Correction

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,3 +1,8 @@
+/**
+ * Prints a string to STDOUT.
+ * @param {String} msg The message to display.
+ * @author Michael Bonyo <https://github.com/maiko254>
+ */
 function displayMessage(data) {
     process.stdout.write(data + '\n');
 }
