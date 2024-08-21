8c8f774ad8625a0468d3efc484313d9d20f2364c
maiko254 <bonyomichael406.com>
2024-08-21 10:15:51 +0300

    Correction: Fixed eslint errors

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,30 +1,29 @@
 const fs = require('fs').promises;
 
 async function countStudents(path) {
-    try {
-        const data = await fs.readFile(path, 'utf8');
-        const lines = data.split('\n').filter((line) => line.length > 0);
-        console.log(`Number of students: ${lines.length === 0 ? 0 : lines.length - 1}`);
-        const fields = {};
-        lines.forEach((line) => {
-            const student = line.split(',');
-            if (fields[student[3]]) {
-                fields[student[3]].push(student[0]);
-            } else {
-                fields[student[3]] = [student[0]];
-            }
-        });
-        delete fields.field;
-        for (const field in fields) {
-            if (field) {
-                const list = fields[field];
-                console.log(`Number of students in ${field}: ${list.length}. List: ${list.join(', ')}`);
-            }
-        }
-    } catch (err) {
-        throw new Error('Cannot load the database');
+  try {
+    const data = await fs.readFile(path, 'utf8');
+    const lines = data.split('\n').filter((line) => line.length > 0);
+    console.log(`Number of students: ${lines.length === 0 ? 0 : lines.length - 1}`);
+    const fields = {};
+    lines.forEach((line) => {
+      const student = line.split(',');
+      if (fields[student[3]]) {
+        fields[student[3]].push(student[0]);
+      } else {
+        fields[student[3]] = [student[0]];
+      }
+    });
+    delete fields.field;
+    for (const field in fields) {
+      if (field) {
+        const list = fields[field];
+        console.log(`Number of students in ${field}: ${list.length}. List: ${list.join(', ')}`);
+      }
     }
-        
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
 }
 
 module.exports = countStudents;
