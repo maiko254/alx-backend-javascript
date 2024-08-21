04468c47034a18e6f0e7d1f1726f81fa87ccb497
maiko254 <bonyomichael406.com>
2024-08-21 10:17:57 +0300

    Correction: Fixed eslint errors

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -4,13 +4,13 @@ const host = 'localhost';
 const port = 1245;
 
 const app = http.createServer((req, res) => {
-    res.setHeader('Content-Type', 'text/plain');
-    res.write('Hello Holberton School!');
-    res.end();
+  res.setHeader('Content-Type', 'text/plain');
+  res.write('Hello Holberton School!');
+  res.end();
 });
 
 app.listen(port, host, () => {
-    console.log(`Server running at http://${host}:${port}`);
+  console.log(`Server running at http://${host}:${port}`);
 });
 
 module.exports = app;
