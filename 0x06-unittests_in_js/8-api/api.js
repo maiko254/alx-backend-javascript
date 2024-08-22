dbc4399fbebcccf9dc15d66ea95e0a17bad026ab
maiko254 <bonyomichael406.com>
2024-08-22 17:10:02 +0300

    Correction

--- 0x06-unittests_in_js/8-api/api.js
+++ 0x06-unittests_in_js/8-api/api.js
@@ -4,14 +4,11 @@ const app = express();
 const port = 7865;
 
 app.get('/', (req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
     res.send('Welcome to the payment system');
-    res.end();
 });
 
 app.listen(port, () => {
-    console.log('API available on localhost port 7865');
+    console.log(`API available on localhost port ${port}`);
 });
 
 module.exports = app;
