3d65205f4e634b8c55933a55140b6d542a800342
maiko254 <bonyomichael406.com>
2024-08-22 13:39:46 +0300

    Testing response from route path with a regex

---
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -0,0 +1,27 @@
+const chai = require('chai');
+const expect = chai.expect;
+const request = require('request');
+const app = require('./api');
+
+describe('api', function () {
+    it('should respond with message "Welcome to the payment system" and status 200', function (done) {
+        request('http://localhost:7865', function (error, response, body) {
+            expect(response.statusCode).to.equal(200);
+            expect(body).to.equal('Welcome to the payment system');
+            done();
+        });
+    });
+    it('should respond with message "Payment methods for cart 1" and status 200', function (done) {
+        request('http://localhost:7865/cart/1', function (error, response, body) {
+            expect(response.statusCode).to.equal(200);
+            expect(body).to.equal('Payment methods for cart 1');
+            done();
+        });
+    });
+    it('should respond with status 404', function (done) {
+        request('http://localhost:7865/cart/mycart', function (error, response, body) {
+            expect(response.statusCode).to.equal(404);
+            done();
+        });
+    });
+});
