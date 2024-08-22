79781239264c2a2c4f8fad929b5be90c0d1577c5
maiko254 <bonyomichael406.com>
2024-08-22 17:05:48 +0300

    Correction: split the tests

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,13 +1,19 @@
-const chai = require('chai');
-const expect = chai.expect;
+// Used to test the API in 8-api/api.js
+const expect = require('chai').expect;
 const request = require('request');
 const app = require('./api');
+
 
 describe('Index page', function () {
     it('should respond with message "Welcome to the payment system" and status 200', function (done) {
         request('http://localhost:7865', function (error, response, body) {
-            expect(response.statusCode).to.equal(200);
             expect(body).to.equal('Welcome to the payment system');
+            done();
+        });
+    });
+    it('should respond with status 200', function (done) {
+        request('http://localhost:7865', function (error, response, body) {
+            expect(response.statusCode).to.equal(200);
             done();
         });
     });
