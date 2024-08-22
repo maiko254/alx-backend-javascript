0bf75fdef03b19304b1f1d58d0c8e0cb9a8646df
maiko254 <bonyomichael406.com>
2024-08-22 15:31:24 +0300

    Correction: rewrote the tests to use expect

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -1,36 +1,37 @@
 const chai = require('chai');
-const assert = chai.assert;
 const calculateNumber = require('./1-calcul');
+
+const expect = chai.expect;
 
 describe('calculateNumber', function() {
     it('should return 4', function() {
-        assert.strictEqual(calculateNumber('SUM', 1.5, 2), 4);
+        expect(calculateNumber('SUM', 1.5, 2)).to.equal(4);
     });
     it('should return 5', function() {
-        assert.strictEqual(calculateNumber('SUM', 1.9, 3), 5);
+        expect(calculateNumber('SUM', 1.9, 3)).to.equal(5);
     });
     it('should return -11', function() {
-        assert.strictEqual(calculateNumber('SUM', -3.5, -8.2), -11);
+        expect(calculateNumber('SUM', -3.5, -8.2)).to.equal(-11);
     });
     it('should return 6', function() {
-        assert.strictEqual(calculateNumber('SUBTRACT', -9.3, -15), 6);
+        expect(calculateNumber('SUBTRACT', -9.3, -15)).to.equal(6);
     });
     it('should return 10', function() {
-        assert.strictEqual(calculateNumber('SUBTRACT', 20.5, 11), 10);
+        expect(calculateNumber('SUBTRACT', 20.5, 11)).to.equal(10);
     });
     it('should return -1', function() {
-        assert.strictEqual(calculateNumber('SUBTRACT', 1.4, 2.5), -2);
+        expect(calculateNumber('SUBTRACT', 1.4, 2.5)).to.equal(-2);
     });
     it('should return 2', function() {
-        assert.strictEqual(calculateNumber('DIVIDE', 8, 4), 2);
+        expect(calculateNumber('DIVIDE', 8, 4)).to.equal(2);
     });
     it('should return 2', function() {
-        assert.strictEqual(calculateNumber('DIVIDE', 8, 3.5), 2);
+        expect(calculateNumber('DIVIDE', 8, 3.5)).to.equal(2);
     });
     it('should return 2.6666666666666665', function() {
-        assert.strictEqual(calculateNumber('DIVIDE', 8, 3.4), 2.6666666666666665);
+        expect(calculateNumber('DIVIDE', 8, 3.4)).to.equal(2.6666666666666665);
     });
     it('should return Error', function() {
-        assert.strictEqual(calculateNumber('DIVIDE', 8, 0), 'Error');
+        expect(calculateNumber('DIVIDE', 8, 0)).to.equal('Error');
     });
 });
