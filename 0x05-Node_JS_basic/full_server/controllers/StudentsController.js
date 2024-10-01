27070181b4f404834814e4974807696c8fd29631
maiko254 <bonyomichael406.com>
2024-10-01 21:13:14 +0300

    Fix: Modified methods in StudentsController to sort fields alphabetically

--- 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
+++ 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -1,49 +1,34 @@
 const readDatabase = require('../utils');
 
-const VALID_MAJORS = ['CS', 'SWE'];
-
 class StudentsController {
   static getAllStudents(request, response) {
     const db = process.argv[2];
 
-    try {
-      response.status(200);
-      readDatabase(db).then((data) => {
-        const dataList = [];
-        for (const field in data) {
-          if (field) {
-            const list = data[field];
-            dataList.push(`Number of students in ${field}: ${list.length}. List: ${list.join(', ')}`);
-          }
-        }
-        response.send(`This is the list of our students\n${dataList.join('\n')}`);
-      }).catch((error) => {
-        response.status(500).send('Cannot load the database');
-      });
-    } catch (error) {
-      console.log(error);
-    }
+    readDatabase(db).then((data) => {
+      const dataList = [];
+      for (const field of Object.keys(data).sort((a, b) => a.localeCompare(b, undefined, { sensitivity: 'base' }))) {
+        const list = data[field];
+        dataList.push(`Number of students in ${field}: ${list.length}. List: ${list.join(', ')}`);
+      }
+      response.status(200).send(`This is the list of our students\n${dataList.join('\n')}`);
+    }).catch(() => {
+      response.status(500).send('Cannot load the database');
+    });
   }
 
   static getAllStudentsByMajor(request, response) {
     const db = process.argv[2];
     const { major } = request.params;
 
-    try {
-      response.status(200);
-      if (!VALID_MAJORS.includes(major)) {
-        response.status(500).send('Major parameter must be CS or SWE');
-        return;
-      }
-
+    if (!['CS', 'SWE'].includes(major)) {
+      response.status(500).send('Major parameter must be CS or SWE');
+    } else {
       readDatabase(db).then((data) => {
-        const studentGroup = data[major];
-        response.send(`List: ${studentGroup.join(',')}`);
-      }).catch((error) => {
+        const studentGroup = data[major] || [];
+        response.status(200).send(`List: ${studentGroup.join(', ')}`);
+      }).catch(() => {
         response.status(500).send('Cannot load the database');
       });
-    } catch (error) {
-      console.log(error);
     }
   }
 }
