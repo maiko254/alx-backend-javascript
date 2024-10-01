6e886bc9db587e63ae058ad53a74cc830b45547b
maiko254 <bonyomichael406.com>
2024-10-01 20:20:03 +0300

    Fixed linter errors

--- 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
+++ 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -2,46 +2,46 @@ const readDatabase = require('../utils');
 const VALID_MAJORS = ['CS', 'SWE'];
 
 class StudentsController {
-    static getAllStudents(request, response) {
-        const db = process.argv[2];
+  static getAllStudents(request, response) {
+    const db = process.argv[2];
 
-        try {
-            response.status(200);
-            readDatabase(db).then((data) => {
-                const dataList = [];
-                for (const field in data) {
-                    if (field) {
-                        const list = data[field];
-                        dataList.push(`Number of students in ${field}: ${list.length}. List: ${list.join(', ')}`);
-                    }
-                }
-                response.send(`This is the list of our students\n${dataList.join('\n')}`);
-            });
-        } catch(error) {
-            response.status(500);
-            response.send('Cannot load the database');
+    try {
+      response.status(200);
+      readDatabase(db).then((data) => {
+        const dataList = [];
+        for (const field in data) {
+          if (field) {
+            const list = data[field];
+            dataList.push(`Number of students in ${field}: ${list.length}. List: ${list.join(', ')}`);
+          }
         }
+        response.send(`This is the list of our students\n${dataList.join('\n')}`);
+      });
+    } catch (error) {
+      response.status(500);
+      response.send('Cannot load the database');
     }
+  }
 
-    static getAllStudentsByMajor(request, response) {
-        const db = process.argv[2];
-        const { major } = request.params;
+  static getAllStudentsByMajor(request, response) {
+    const db = process.argv[2];
+    const { major } = request.params;
 
-        try {
-            response.status(200);
-            if (!VALID_MAJORS.includes(major)) {
-                response.status(500).send('Major parameter must be CS or SWE');
-                return;
-            }
+    try {
+      response.status(200);
+      if (!VALID_MAJORS.includes(major)) {
+        response.status(500).send('Major parameter must be CS or SWE');
+        return;
+      }
 
-            readDatabase(db).then((data) => {
-                const studentGroup = data[major];
-                response.send(`List: ${studentGroup.join(',')}`);
-            });
-        } catch(error) {
-            response.status(500).send('Cannot load the database');
-        }
+      readDatabase(db).then((data) => {
+        const studentGroup = data[major];
+        response.send(`List: ${studentGroup.join(',')}`);
+      });
+    } catch (error) {
+      response.status(500).send('Cannot load the database');
     }
+  }
 }
 
 module.exports = StudentsController;
--- 0x05-Node_JS_basic/full_server/utils.js
+++ 0x05-Node_JS_basic/full_server/utils.js
@@ -1,27 +1,27 @@
 const fs = require('fs').promises;
 
 async function readDatabase(path) {
-    const dataPromise = new Promise((resolve, reject) => {
-        fs.readFile(path, 'utf8')
-            .then((data) => {
-                const lines = data.split('\n').filter((line) => line.length > 0);
-                const fields = {};
-                lines.forEach((line) => {
-                    const student = line.split(',');
-                    if (fields[student[3]]) {
-                        fields[student[3]].push(student[0]);
-                    } else {
-                        fields[student[3]] = [student[0]];
-                    }
-                });
-                delete fields.field;
-                resolve(fields);
-            })
-            .catch((error) => {
-                reject(error);
-            });
-    });
-    return dataPromise;
+  const dataPromise = new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8')
+      .then((data) => {
+        const lines = data.split('\n').filter((line) => line.length > 0);
+        const fields = {};
+        lines.forEach((line) => {
+          const student = line.split(',');
+          if (fields[student[3]]) {
+            fields[student[3]].push(student[0]);
+          } else {
+            fields[student[3]] = [student[0]];
+          }
+        });
+        delete fields.field;
+        resolve(fields);
+      })
+      .catch((error) => {
+        reject(error);
+      });
+  });
+  return dataPromise;
 }
 
 module.exports = readDatabase;
