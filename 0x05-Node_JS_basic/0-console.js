f681facd87ca48dc9601df6ed45b3eb24390f802
maiko254 <bonyomichael406.com>
2024-10-01 11:31:37 +0300

    NodeJS backend server

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Prints a string to STDOUT.
+ * @param {String} msg The message to display.
+ * @author Michael Bonyo <https://github.com/maiko254>
+ */
+const displayMessage = (data) => {
+  console.log(data);
+};
+
+module.exports = displayMessage;
