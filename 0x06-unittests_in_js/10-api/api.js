5d5ab1991ef48e647467a813e0e63d0ab01c1cb1
maiko254 <bonyomichael406.com>
2024-10-03 18:13:30 +0300

    Deep equality & Post integration testing

---
+++ 0x06-unittests_in_js/10-api/api.js
@@ -0,0 +1,46 @@
+const express = require('express');
+
+const app = express();
+const port = 7865;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.send('Welcome to the payment system');
+    res.end();
+});
+
+app.get('/cart/:id([0-9]+)', (req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.send(`Payment methods for cart ${req.params.id}`);
+    res.end();
+});
+
+app.get('/available_payments', (req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.json({
+        payment_methods: {
+            credit_card: true,
+            paypal: false
+        }
+    }
+            
+    );
+    res.end();
+});
+
+app.post('/login', (req, res) => {
+    res.statusCode = 200;
+    res.send(`Welcome ${req.body.userName}`);
+    res.end();
+});
+
+app.listen(port, () => {
+    console.log(`API available on localhost port ${port}`);
+});
+
+module.exports = app;
---
+++ 0x06-unittests_in_js/10-api/api.test.js
@@ -0,0 +1,46 @@
+const chai = require('chai');
+const expect = chai.expect;
+const request = require('request');
+
+describe('Index page', function () {
+    it('should respond with message "Welcome to the payment system" and status 200', function (done) {
+        request('http://localhost:7865', function (error, response, body) {
+            expect(response.statusCode).to.equal(200);
+            expect(body).to.equal('Welcome to the payment system');
+            done();
+        });
+    });
+});
+
+describe('Cart page', function () {
+    it('should respond with message "Payment methods for cart 1" and status 200', function (done) {
+        request('http://localhost:7865/cart/1', function (error, response, body) {
+            expect(response.statusCode).to.equal(200);
+            expect(body).to.equal('Payment methods for cart 1');
+            done();
+        });
+    });
+    it('should respond with status 404', function (done) {
+        request('http://localhost:7865/cart/mycart', function (error, response, body) {
+            expect(response.statusCode).to.equal(404);
+            done();
+        });
+    });
+});
+
+describe('Available_payments page', function () {
+    it('should respond with message "Welcome NewUser" and status 200', function (done) {
+        request.post('http://localhost:7865/login', { json: { userName: 'NewUser' } }, function (error, response, body) {
+            expect(response.statusCode).to.equal(200);
+            expect(body).to.equal('Welcome NewUser');
+            done();
+        });
+    });
+    it('should respond with JSON object and status 200', function (done) {
+        request('http://localhost:7865/available_payments', function (error, response, body) {
+            expect(response.statusCode).to.equal(200);
+            expect(JSON.parse(body)).to.be.deep.equal({payment_methods:{ credit_card: true, paypal: false }});
+            done();
+        });
+    });
+});
